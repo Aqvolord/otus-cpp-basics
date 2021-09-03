9e9fb0bd36d188ee95ca040d7ed2c8d043c5f123
Алексей <@WIN-V0826VOBIBP>
2021-09-03 16:57:50 +0300

    2

--- statistics.cpp
+++ statistics.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <limits>
+#include<cmath>
 
-class IStatistics {
+class IStatistics
+{
 public:
-	virtual void change(float next) = 0;
+	virtual void change(float num) = 0;
 	virtual float getMeaning() const = 0;
 	virtual const char* getName() const = 0;
 	virtual ~IStatistics() {}
 };
 
 
-class Min : public IStatistics {
+class Min : public IStatistics
+{
 public:
-	Min() : _min{ std::numeric_limits<float>::min() } {}
+	Min() : _min{ std::numeric_limits<float>::max() } {}
 
 	void change(float num) override
 	{
-		if (num < _min)
+		if (_min > num)
 			_min = num;
 	}
 
@@ -32,12 +35,13 @@ class Max : public IStatistics
 public:
 	void change(float num) override
 	{
-		if (num > _max)
+		if (_max < num)
 			_max = num;
 	}
 
 	float getMeaning() const override { return _max; }
 	const char* getName() const override { return "max"; }
+
 private:
 	float _max;
 };
@@ -45,42 +49,95 @@ private:
 class Mean : public IStatistics
 {
 public:
+	void change(float num) override
+	{
+	_sum += num;
+	_ordinal++;
+	_mean = _sum / _ordinal;
+	}
+	
+	float getMeaning() const override { return _mean; }
+	int getOrdinal()const { return _ordinal; }
+	const char* getName() const override { return "mean"; }
+
 private:
+	float _sum;
+	int _ordinal;
 	float _mean;
 };
 
 class Std : public IStatistics
 {
 public:
+	void change(float num) override
+	{
+		//_num = num;
+		/*_sum += num;
+		_ordinal++;
+		_mean = _sum / _ordinal;
+
+		_sum += pow((num - _mean), 2);
+		_std = sqrt(_sum / _ordinal);*/
+	}
+
+	float getMeaning() const override { return _std; }
+	const char* getName() const override { return "std"; }
+
 private:
-	float _stad;
+	//float _sum;
+	//float _num;
+	//float _mean;
+	//int _ordinal;
+	float _std;
 };
 
-class Pct90 : public IStatistics
-{
-public:
-private:
-	float _pct90;
-};
-
-class Pct50 : public IStatistics
-{
-public:
-private:
-	float _pct50;
-};
+//class Pct90 : public IStatistics
+//{
+//public:
+//	void change(float num) override
+//	{
+//
+//	}
+//
+//	float getMeaning() const override { return _pct90; }
+//	const char* getName() const override { return "pct90"; }
+//
+//private:
+//	float _pct90;
+//};
+//
+//class Pct50 : public IStatistics
+//{
+//public:
+//	void change(float num) override
+//	{
+//
+//	}
+//
+//	float getMeaning() const override { return _pct50; }
+//	const char* getName() const override { return "pct50"; }
+//
+//private:
+//	float _pct50;
+//};
 
 int main() {
 
-	const size_t statistics_count = 1;
+	const size_t statistics_count = 6;
 	IStatistics* statistics[statistics_count];
 
 	statistics[0] = new Min{};
+	statistics[1] = new Max{};
+	statistics[2] = new Mean{};
+	statistics[3] = new Std{};
+	//statistics[4] = new Pct90{};
+	//statistics[5] = new Pct50{};
 
-	float val = 0;
-	while (std::cin >> val) {
+	std::cout << "Enter a sequence of numbers: " << std::endl;
+	float value;
+	while (std::cin >> value) {
 		for (size_t i = 0; i < statistics_count; ++i) {
-			statistics[i]->change(val);
+			statistics[i]->change(value);
 		}
 	}
 
