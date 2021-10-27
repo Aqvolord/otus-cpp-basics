2b771f890ef7377884fe37620e66a8b17c56c883
Алексей Комиссаров <KomissarovRUS@mail.ru>
2021-10-27 17:19:54 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "serialContainer.cpp"
+#include "listContainer.cpp"
+
+int main()
+{
+	serialContainer conteiner(0, 10);
+	conteiner.SizeContainer();
+	conteiner.DeleteElements();
+	conteiner.AddingElements();
+
+
+
+	return 0;
+}
